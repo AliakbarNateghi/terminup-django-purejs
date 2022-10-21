f728375f556c45f40d187e83ecabc87bc159614f
AliakbarNateghi <88666781+AliakbarNateghi@users.noreply.github.com>
2022-10-21 15:09:06 -0400

    Add files via upload

--- shariz/settings.py
+++ shariz/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os.path
 from pathlib import Path
-
 from django.contrib import staticfiles
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -27,7 +26,7 @@ SECRET_KEY = 'django-insecure-e3k40-n$2ezw&b-znl)d7am0xd5mv+wg+d95honpv4&m3lnkvz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOST = ['*']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -122,7 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = [
     os.path.join(STATIC_ROOT, 'static'),
 ]
