c91a6e5d7ccce014b5c37e1f1ae6b4c947fb4731
AliakbarNateghi <88666781+AliakbarNateghi@users.noreply.github.com>
2022-10-22 18:37:19 +0330

    Create settings.py

--- shariz/settings.py
+++ shariz/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e3k40-n$2ezw&b-znl)d7am0xd5mv+wg+d95honpv4&m3lnkvz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['terminup.herokuapp.com', '127.0.0.1', 'termina.up.railway.app']
 
